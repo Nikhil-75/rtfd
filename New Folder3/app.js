63183b3f9c4ef85549623cc0123a3177d8176b17
aryan <aryanpal692@gmail.com>
2023-02-13 19:11:32 +0530

    data insert

---
+++ New Folder3/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const dbConnect = require('./mongodb')
+
+app.use(express.json())
+
+app.post('/', async (req,resp)=> {
+
+   let data = await dbConnect("users", req.body);
+   console.log(data)
+   // console.log(req.body
+   resp.json({
+    data:data})
+})
+
+app.listen(4000, ()=>console.log("server is up"));
